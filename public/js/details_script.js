f14c3e0497404e2178c1912ad5435f117b9c08a4
discotechnology <drosenfeld@csumb.edu>
2025-12-15 20:35:45 -0800

    Added endpoints to add/delete repairs, added "add repair" button and modal

--- public/js/details_script.js
+++ public/js/details_script.js
@@ -1,7 +1,15 @@
 // Event Listeners
 document.querySelector('#editDetails').addEventListener('click', detailsModal);
+document.querySelector('#addRepair').addEventListener('click', addRepairModal);
 
+
+// Functions
 async function detailsModal() {
     var myModal = new bootstrap.Modal(document.getElementById('detailsModal'));
+    myModal.show();
+}
+
+async function addRepairModal() {
+    var myModal = new bootstrap.Modal(document.getElementById('addRepairModal'));
     myModal.show();
 }
